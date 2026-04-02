2a259ff7958f13ad583a53ba01e95a4ac8cdee93
Vlad <vlad08052@mail.ru>
2026-04-02 15:18:48 +0200

    Category Class + Tests + Small Changes in Transaction

--- src/main/java/com/finance/FinanceApp/Transaction/Transaction.java
+++ src/main/java/com/finance/FinanceApp/Transaction/Transaction.java
@@ -1,10 +1,18 @@
 package com.finance.FinanceApp.Transaction;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private BigDecimal amount;
@@ -12,12 +20,10 @@ public class Transaction {
 
     public Transaction(){}
 
-    public Transaction(long id, String title, BigDecimal amount, LocalDateTime date){
-        if(id < 0) throw new IllegalArgumentException("Id must be positive");
+    public Transaction(String title, BigDecimal amount, LocalDateTime date){
         if(title == null || title.isBlank()) throw new IllegalArgumentException("Title must have name");
         if(amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Amount must be positive");
         if(date == null) throw new NullPointerException("Date cannot be null");
-        this.id = id;
         this.title = title;
         this.amount = amount;
         this.date = date;
@@ -37,5 +43,20 @@ public class Transaction {
 
     public LocalDateTime getDate() {
         return date;
+    }
+
+    public void setTitle(String title){
+        if(title == null || title.isBlank()) throw new IllegalArgumentException("Title must have name");
+        this.title = title;
+    }
+
+    public void setAmount(BigDecimal amount){
+        if(amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Amount must be positive");
+        this.amount = amount;
+    }
+
+    public void setDate(LocalDateTime date){
+        if(date == null) throw new NullPointerException("Date cannot be null");
+        this.date = date;
     }
 }
