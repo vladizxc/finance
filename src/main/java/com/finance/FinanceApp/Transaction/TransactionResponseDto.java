2b54e9f08bc3adc21a06a721d207b613f214afdf
Vlad <vlad08052@mail.ru>
2026-04-05 13:33:25 +0200

    TransactionResopnseDto complete and tests

--- src/main/java/com/finance/FinanceApp/Transaction/TransactionResponseDto.java
+++ src/main/java/com/finance/FinanceApp/Transaction/TransactionResponseDto.java
@@ -11,10 +11,64 @@ public class TransactionResponseDto {
     private String categoryName;
 
     public TransactionResponseDto(Long id, String title, BigDecimal amount, LocalDateTime date, String categoryName){
+        if(id == null || id < 0)
+            throw new IllegalArgumentException("Id cannot be null or negative");
+        if(title == null || title.isBlank()) throw new IllegalArgumentException("Title must have name");
+        if(amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Amount must be positive");
+        if(date == null) throw new IllegalArgumentException("Date cannot be null");
+        if (categoryName == null || categoryName.isBlank())
+            throw new IllegalArgumentException("CategoryName cannot be null or blank");
         this.id = id;
         this.title = title;
         this.amount = amount;
         this.date = date;
+        this.categoryName = categoryName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        if(id == null || id < 0)
+            throw new IllegalArgumentException("Id cannot be null or negative");
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if(title == null || title.isBlank()) throw new IllegalArgumentException("Title must have name");
+        this.title = title;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        if(amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Amount must be positive");
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        if(date == null) throw new IllegalArgumentException("Date cannot be null");
+        this.date = date;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        if (categoryName == null || categoryName.isBlank())
+            throw new IllegalArgumentException("CategoryName cannot be null or blank");
         this.categoryName = categoryName;
     }
 }
